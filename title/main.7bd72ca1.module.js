a445237bf48955e2c45d919b7ef956dab033d081
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-12-03 12:00:31 +0700

    Updates

--- title/main.7bd72ca1.module.js
+++
@@ -1,2 +0,0 @@
-import{a4 as e,n as o}from"../js/lib.a89aa200.module.js";import{C as s}from"../js/wesib.51c5f70c.module.js";import{B as a,A as m,e as l}from"../js/common.8ecb5c1c.module.js";let t=class{};t=e([s({name:["title",a],feature:{needs:[m]}})],t),l.load(t)(o).whenOff(e=>{console.error("Failed to load title component",e)});
-//# sourceMappingURL=main.7bd72ca1.module.js.map
---
+++ title/main.8699a065.module.js
@@ -0,0 +1,2 @@
+import{a4 as o,n as a}from"../js/lib.a89aa200.module.js";import{C as e}from"../js/wesib.3f4b5cdb.module.js";import{B as s,A as m,e as l}from"../js/common.b6a9b8a4.module.js";let t=class{};t=o([e({name:["title",s],feature:{needs:[m]}})],t),l.load(t)(a).whenOff(o=>{console.error("Failed to load title component",o)});
+//# sourceMappingURL=main.8699a065.module.js.map
