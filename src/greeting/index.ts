ad3332d5906a8851c31006f14a7d433f1200e557
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-12-08 18:16:49 +0700

    Version bump

--- src/greeting/index.ts
+++ src/greeting/index.ts
@@ -1,7 +1,4 @@
-import { noop } from 'call-thru';
 import { examplesContext } from '../common';
 import { GreetingComponent } from './greeting.component';
 
-examplesContext.load(GreetingComponent)(noop).whenOff(reason => {
-  console.error('Failed to load greet-text example', reason);
-});
+examplesContext.load(GreetingComponent);
--- src/title/index.ts
+++ src/title/index.ts
@@ -1,7 +1,4 @@
-import { noop } from 'call-thru';
 import { examplesContext } from '../common';
 import { TitleComponent } from './title.component';
 
-examplesContext.load(TitleComponent)(noop).whenOff(reason => {
-  console.error('Failed to load title component', reason);
-});
+examplesContext.load(TitleComponent);
