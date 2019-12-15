ab53f3930fe42bbca3470da21c470dbb0ea97c94
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-12-15 16:18:51 +0700

    Updates

--- title/main.8b0d1757.module.js
+++
@@ -1,2 +0,0 @@
-import{a5 as s}from"../js/lib.a5838606.module.js";import{C as a}from"../js/wesib.53f2dfad.module.js";import{B as e,A as m,e as o}from"../js/common.db874bb8.module.js";let d=class{};d=s([a({name:["title",e],feature:{needs:[m]}})],d),o.load(d);
-//# sourceMappingURL=main.8b0d1757.module.js.map
