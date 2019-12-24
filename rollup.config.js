ff292d7702fd629210e9bca370f6b21a297e9ac5
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-12-24 13:21:56 +0700

    Version bump

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import 'ts-node/register';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
 import sourcemaps from 'rollup-plugin-sourcemaps';
-import nodeResolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
