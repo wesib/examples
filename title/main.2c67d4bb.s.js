3bc8db15a1f96d1d9a5593475aff5315d12dd85b
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-12-30 15:43:00 +0700

    Updates

--- title/main.2c67d4bb.s.js
+++
@@ -1,2 +0,0 @@
-System.register(["../js/lib.8cb6a86d.s.js","../js/wesib.5e383b75.s.js","../js/common.7d34b50d.s.js"],(function(){"use strict";var e,n,t,s,i;return{setters:[function(n){e=n.an},function(e){n=e.C},function(e){t=e.E,s=e.A,i=e.e}],execute:function(){var u=function(){function i(){}return i=e([n({name:["title",t],feature:{needs:[s]}})],i)}();i.load(u)}}}));
-//# sourceMappingURL=main.2c67d4bb.s.js.map
--- title/main.af7eb423.m.js
+++
@@ -1,2 +0,0 @@
-import{a7 as s}from"../js/lib.e2c9a0a7.m.js";import{C as a}from"../js/wesib.0ecb839b.m.js";import{E as m,A as e,e as o}from"../js/common.b5d86bc5.m.js";let r=class{};r=s([a({name:["title",m],feature:{needs:[e]}})],r),o.load(r);
-//# sourceMappingURL=main.af7eb423.m.js.map
