f34c91334f5b330d4126ae7a66d778630bbdddfd
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-01-16 13:22:52 +0700

    Updates

--- title/main.2571ac28.m.js
+++
@@ -1,2 +0,0 @@
-import{a8 as s}from"../js/lib.b60fe0fe.m.js";import{C as a}from"../js/wesib.bbda94b4.m.js";import{E as e,A as m,e as o}from"../js/common.1ad655ea.m.js";let r=class{};r=s([a({name:["title",e],feature:{needs:[m]}})],r),o.load(r);
-//# sourceMappingURL=main.2571ac28.m.js.map
---
+++ title/main.b7c5524f.s.js
@@ -0,0 +1,2 @@
+System.register(["../js/lib.84c74c4f.s.js","../js/wesib.6c2e977e.s.js","../js/common.4b7de1ad.s.js"],(function(){"use strict";var e,n,t,s,c;return{setters:[function(n){e=n.ao},function(e){n=e.C},function(e){t=e.E,s=e.A,c=e.e}],execute:function(){var i=function(){function c(){}return c=e([n({name:["title",t],feature:{needs:[s]}})],c)}();c.load(i)}}}));
+//# sourceMappingURL=main.b7c5524f.s.js.map
