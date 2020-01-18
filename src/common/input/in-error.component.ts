236d3aa5f889452f9d6798c148192a093083afc0
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-01-18 12:57:37 +0700

    Utilize `inCssError()`
    Instead of custom rendering

--- src/common/input/in-error.component.ts
+++ src/common/input/in-error.component.ts
@@ -1,15 +1,8 @@
-import { HierarchyContext, InputFromControl, inputValidity, ProduceStyle, Theme } from '@wesib/generic';
-import {
-  AttributeChanged,
-  Component,
-  ComponentContext,
-  DefaultNamespaceAliaser,
-  DefaultRenderScheduler,
-  Render,
-} from '@wesib/wesib';
-import { itsEvery } from 'a-iterable';
-import { InCssClasses, InputAspects__NS, InValidation, inValidationResult } from 'input-aspects';
-import { css__naming, QualifiedName } from 'namespace-aliaser';
+import { DefaultInAspects, HierarchyContext, InputFromControl, ProduceStyle, Theme } from '@wesib/generic';
+import { AttributeChanged, Component, ComponentContext } from '@wesib/wesib';
+import { afterAll, eventSupply, trackValue } from 'fun-events';
+import { InCssClasses, inCssError, inCssInfo, InputAspects__NS, InStyledElement } from 'input-aspects';
+import { QualifiedName } from 'namespace-aliaser';
 import { StypLengthPt, stypRules, StypRules } from 'style-producer';
 import { Examples__NS } from '../examples.ns';
 import { FormThemeSettings } from './form.theme-settings';
@@ -17,54 +10,44 @@ import { FormThemeSettings } from './form.theme-settings';
 @Component(['in-error', Examples__NS])
 export class InErrorComponent {
 
-  private _validity: InValidation.Result = inValidationResult();
-  private _codes = new Set<string>();
+  private readonly _codes = trackValue<string[]>();
 
   constructor(private readonly _context: ComponentContext) {
-    inputValidity(_context)(validity => this.validity = validity);
-    this._context.get(HierarchyContext).get(InputFromControl).consume(
-        ({ control }) => control && control.aspect(InCssClasses)
-            .applyTo(
-                _context.element,
-                _context.get(DefaultRenderScheduler)(),
-            ),
+    afterAll({
+      control: this._context.get(HierarchyContext).get(InputFromControl),
+      aspects: this._context.get(DefaultInAspects),
+      when: this._codes,
+    }).consume(
+        ({
+          control: [{ control }],
+          aspects: [aspects],
+          when: [when],
+        }) => {
+          if (!control) {
+            return;
+          }
+
+          const supply = eventSupply();
+
+          control.convert(
+              InStyledElement.to(_context.element),
+              aspects,
+          ).setup(
+              InCssClasses,
+              cssClasses => {
+                cssClasses.add(inCssInfo()).needs(supply);
+                cssClasses.add(inCssError({ when })).needs(supply);
+              },
+          );
+
+          return supply;
+        },
     );
-  }
-
-  get validity(): InValidation.Result {
-    return this._validity;
-  }
-
-  set validity(value: InValidation.Result) {
-
-    const oldValue = this._validity;
-
-    this._validity = value;
-    this._context.updateState('validity', value, oldValue);
   }
 
   @AttributeChanged('code')
   setCode(code: string) {
-    this._codes = new Set(code ? code.trim().split(/\s+/) : []);
-  }
-
-  @Render()
-  render() {
-
-    const element: Element = this._context.element;
-    const { classList } = element;
-    const hasErrorsClassName = css__naming.name(
-        hasError__cssClass,
-        this._context.get(DefaultNamespaceAliaser),
-    );
-
-    return () => {
-      if (itsEvery(this._codes, code => this.validity.has(code))) {
-        classList.add(hasErrorsClassName);
-      } else {
-        classList.remove(hasErrorsClassName);
-      }
-    };
+    this._codes.it = code ? code.trim().split(/\s+/) : [];
   }
 
   @ProduceStyle()
@@ -75,7 +58,7 @@ export class InErrorComponent {
 }
 
 const InError__qualifier = 'bex:in-error';
-const hasError__cssClass: QualifiedName = ['has-error', Examples__NS];
+const hasError__cssClass: QualifiedName = ['has-error', InputAspects__NS];
 
 function InErrorStyle(theme: Theme): StypRules {
 
@@ -99,7 +82,7 @@ function InErrorStyle(theme: Theme): StypRules {
             u: [
               ':', 'host',
               {
-                c: hasError__cssClass,
+                c: [hasError__cssClass, ['touched', InputAspects__NS]],
                 u: [':', 'not', { c: ['has-focus', InputAspects__NS] }],
               },
             ],
