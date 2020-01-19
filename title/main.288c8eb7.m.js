525d5c0dd4e2c56b5cc5d775e4264ff01a166043
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-01-19 16:15:55 +0700

    Updates

---
+++ title/main.288c8eb7.m.js
@@ -0,0 +1,2 @@
+import{aa as s}from"../js/lib.072bc1da.m.js";import{C as a}from"../js/wesib.025b6406.m.js";import{E as m,A as e,e as o}from"../js/common.475ebc99.m.js";let r=class{};r=s([a({name:["title",m],feature:{needs:[e]}})],r),o.load(r);
+//# sourceMappingURL=main.288c8eb7.m.js.map
---
+++ title/main.636b487d.s.js
@@ -0,0 +1,2 @@
+System.register(["../js/lib.72325879.s.js","../js/wesib.86a10da4.s.js","../js/common.d25f16d4.s.js"],(function(){"use strict";var e,n,t,s,i;return{setters:[function(n){e=n.ao},function(e){n=e.C},function(e){t=e.E,s=e.A,i=e.e}],execute:function(){var u=function(){function i(){}return i=e([n({name:["title",t],feature:{needs:[s]}})],i)}();i.load(u)}}}));
+//# sourceMappingURL=main.636b487d.s.js.map
