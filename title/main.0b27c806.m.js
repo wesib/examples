6bd3c4a29b908cdd0b1a374fc3b30375c7b2ad4e
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-01-28 16:12:28 +0700

    Updates

--- title/main.0b27c806.m.js
+++
@@ -1,1 +0,0 @@
-import{aa as s}from"../js/lib.f35d7321.m.js";import{C as m}from"../js/wesib.b3993c14.m.js";import{E as a,A as e,e as o}from"../js/common.9b0b881c.m.js";let r=class{};r=s([m({name:["title",a],feature:{needs:[e]}})],r),o.load(r);//# sourceMappingURL=main.0b27c806.m.js.map
