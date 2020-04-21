29df233a024078ea420b2cfd1d8fe57c446df864
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-04-21 19:01:05 +0700

    Version bump

--- src/common/theme/body-style-support.feature.ts
+++ src/common/theme/body-style-support.feature.ts
@@ -1,13 +1,16 @@
-import { produceBasicStyle } from '@proc7ts/style-producer';
-import { Theme, ThemeSupport } from '@wesib/generic/styp';
+import { produceBasicStyle, stypDomFormat } from '@proc7ts/style-producer';
+import { ComponentStypDomFormat, ComponentStypFormat, Theme, ThemeSupport } from '@wesib/generic/styp';
 import { Feature } from '@wesib/wesib';
 import { BodyStyle } from './body.style';
 
 @Feature({
   needs: ThemeSupport,
+  setup(setup) {
+    setup.perComponent({ a: ComponentStypFormat, as: ComponentStypDomFormat });
+  },
   init(context) {
     context.whenReady(() => {
-      produceBasicStyle(context.get(Theme).style(BodyStyle));
+      produceBasicStyle(context.get(Theme).style(BodyStyle), stypDomFormat());
     });
   },
 })
