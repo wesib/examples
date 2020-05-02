e0e869ec128581ef037a49235a88858bf54b069c
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-05-02 11:36:00 +0700

    Delay offline style rendering
    To prevent DOM manipulations inside constructor.

--- src/common/theme/body-style-support.feature.ts
+++ src/common/theme/body-style-support.feature.ts
@@ -1,17 +1,12 @@
 import { produceBasicStyle, stypDomFormat } from '@proc7ts/style-producer';
 import { ComponentStypDomFormat, ComponentStypFormat, Theme, ThemeSupport } from '@wesib/generic/styp';
-import { ComponentContext, Feature } from '@wesib/wesib';
+import { Feature } from '@wesib/wesib';
 import { BodyStyle } from './body.style';
 
 @Feature({
   needs: ThemeSupport,
   setup(setup) {
-    setup.perComponent({
-      a: ComponentStypFormat,
-      by(context: ComponentContext) {
-        return new ComponentStypDomFormat(context, { offline: false });
-      },
-    });
+    setup.perComponent({ a: ComponentStypFormat, as: ComponentStypDomFormat });
   },
   init(context) {
     context.whenReady(() => {
