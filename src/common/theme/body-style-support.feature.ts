f593c6167c110fe6fa98e9b7a30cb46d23d0d9f0
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-05-02 10:41:50 +0700

    Do not generate styles offline
    This is broken, because DOM manipulations are prohibited inside
    custom element constructor.

--- src/common/theme/body-style-support.feature.ts
+++ src/common/theme/body-style-support.feature.ts
@@ -1,12 +1,17 @@
 import { produceBasicStyle, stypDomFormat } from '@proc7ts/style-producer';
 import { ComponentStypDomFormat, ComponentStypFormat, Theme, ThemeSupport } from '@wesib/generic/styp';
-import { Feature } from '@wesib/wesib';
+import { ComponentContext, Feature } from '@wesib/wesib';
 import { BodyStyle } from './body.style';
 
 @Feature({
   needs: ThemeSupport,
   setup(setup) {
-    setup.perComponent({ a: ComponentStypFormat, as: ComponentStypDomFormat });
+    setup.perComponent({
+      a: ComponentStypFormat,
+      by(context: ComponentContext) {
+        return new ComponentStypDomFormat(context, { offline: false });
+      },
+    });
   },
   init(context) {
     context.whenReady(() => {
