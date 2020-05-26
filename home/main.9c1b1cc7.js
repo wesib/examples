92fdbb518c55ee9a83cd72ec015f86b33d4a96c2
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-05-26 14:40:59 +0700

    Updates

---
+++ home/main.9c1b1cc7.js
@@ -0,0 +1,1 @@
+import{C as s,E as a,o as e,q as o}from"../js/common.dc8bfe32.js";import{_ as m}from"../js/lib.05c728da.js";let r=(()=>{let o=class{};return o=m([s({name:["home",a],feature:{needs:[e]}})],o),o})();o.load(r);//# sourceMappingURL=main.9c1b1cc7.js.map
