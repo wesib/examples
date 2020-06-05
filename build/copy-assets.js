2965211c485343e423a2216e71bedcb9f3c18a1b
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-06-05 10:41:54 +0700

    Utilize shx for `clean` task

--- build/copy-assets.js
+++ build/copy-assets.js
@@ -6,7 +6,6 @@ const jsDir = path.resolve(rootDir, 'dist', 'js');
 
 (async () => {
   try {
-
     await fs.ensureDir(jsDir);
     await fs.copy(require.resolve('systemjs/dist/s.min.js'), path.resolve(jsDir, 's.js'));
   } catch (e) {
