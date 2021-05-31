33522af8549e99e850ef25d4915535c6f096eec0
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-05-31 16:05:19 +0700

    Updates

--- assets/home.9d4ddd4e.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,C as s}from"./vendor.d0de1fdb.js";import{E as d,A as a}from"./index.901d4f6d.js";let m=class{};m=e([s({name:["home",d],feature:{needs:[a]}})],m);
-//# sourceMappingURL=home.9d4ddd4e.js.map
