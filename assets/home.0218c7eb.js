16c5d9071df4797a724ab3753c34289975b2d493
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-07-12 18:08:13 +0700

    Updates

--- assets/home.0218c7eb.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./vendor.749ed6ff.js";import{C as s,E as a,A as m}from"./index.24d5e639.js";let o=class{};o=e([s({name:["home",a],feature:{needs:[m]}})],o);
-//# sourceMappingURL=home.0218c7eb.js.map
---
+++ assets/home.15a10004.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./vendor.be95cfba.js";import{C as s,E as a,A as m}from"./index.9f456528.js";let o=class{};o=e([s({name:["home",a],feature:{needs:[m]}})],o);
+//# sourceMappingURL=home.15a10004.js.map
