e96e450010bed7130fe6e72bcf6a7ae1bbf08444
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-07-15 21:53:37 +0700

    Updates

---
+++ assets/home.f4801473.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./vendor.be95cfba.js";import{C as a,E as s,A as m}from"./index.7bc8d3ca.js";let o=class{};o=e([a({name:["home",s],feature:{needs:[m]}})],o);
+//# sourceMappingURL=home.f4801473.js.map
